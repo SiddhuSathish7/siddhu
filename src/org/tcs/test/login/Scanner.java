53f916fecfde0b574c870f5333f1bf5e4a811e46
SiddhuSathish7 <sportivesathish07@gmail.com>
2021-04-28 15:11:10 +0530

    added class1

--- src/org/tcs/test/login/Scanner.java
+++ src/org/tcs/test/login/Scanner.java
@@ -16,6 +16,8 @@ public class Scanner {
 		System.out.println("Emp Gender is "+empGender);
 		String empCity = "Chennai";
 		System.out.println("Emp City is "+empCity);
+		String empPlace="salem";
+		System.out.println("Emp Place is "+ empPlace);
 		
 	}
 
